e097143a0e2d9c43e157b0aed67f00c1a1e87fc0
Alina <alina.spring.sadrtdinova@gmail.com>
2026-03-02 15:49:30 +0300

    Создание главного экрана, экрана настроек и навигация между ними

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageSearch = findViewById<com.google.android.material.button.MaterialButton>(R.id.search)
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener{
+            override fun onClick(v: View?){
+                val toSearchDisplay = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(toSearchDisplay)
+            }
+        }
+        imageSearch.setOnClickListener(imageClickListener)
+
+        val imageLibrary = findViewById<com.google.android.material.button.MaterialButton>(R.id.media_library)
+        imageLibrary.setOnClickListener {
+            val toMedia_library = Intent(this, Media_library::class.java)
+            startActivity(toMedia_library)
+        }
+
+        val imageSettings = findViewById<com.google.android.material.button.MaterialButton>(R.id.settings)
+        imageSettings.setOnClickListener {
+            val toSettingsDisplay = Intent(this, SettingsActivity::class.java)
+            startActivity(toSettingsDisplay)
+        }
+
+
     }
 }
